1c599fabe7498a91b848d74ba0c7365f98d9fe89
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-06-06 00:18:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown(
     <div style="background-color: white; padding: 20px 30px; border-radius: 8px; margin-bottom: 30px; box-shadow: 0 2px 6px rgba(0,0,0,0.1);">
         <div style="display: flex; align-items: center; gap: 20px;">
             <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=304,fit=crop,q=95/Aq2B471lDpFnv1BK/logo---trescon-30-anos-mv0jg6Lo2EiV7yLp.png" style="height: 60px;">
-            <h1 style="margin: 0; font-size: 2.4em;">Conciliador Financeiro x Contábil</h1>
+            <h1 style="margin: 0; font-size: 2.4em;">Conciliador</h1>
         </div>
     </div>
     """,
